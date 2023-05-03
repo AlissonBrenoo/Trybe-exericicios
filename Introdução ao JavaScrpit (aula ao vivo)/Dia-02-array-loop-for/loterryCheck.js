aaeb38732ba22b854bf554ef326d02be49460b5d
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-03 16:48:13 -0300

    Inicio da aula ao vivo

---
+++ Introdução ao JavaScrpit (aula ao vivo)/Dia-02-array-loop-for/loterryCheck.js
@@ -0,0 +1,11 @@
+let jogoDoChimba = [3,7,22,12,15,53];
+
+// console.log(jogoDoChimba);
+// console.log(jogoDoChimba[0]);
+// console.log(jogoDoChimba[1]);
+// console.log(jogoDoChimba[2]);
+// console.log(jogoDoChimba[3]);
+// console.log(jogoDoChimba[4]);
+// console.log(jogoDoChimba[5]);
+
+console.log(jogoDoChimba.length);
