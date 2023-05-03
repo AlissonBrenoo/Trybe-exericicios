1e6071afae87af74972b61aa32514b499ba5cfe4
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-03 20:06:56 -0300

    falha ao fazer o exercicio

---
+++ Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
@@ -0,0 +1,10 @@
+let numberss = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = []
+for(let index = 0; index < numberss.length; ){
+    console.log(numberss);
+}
+
+
+// for(let index = 0; index < numberss.length; index+=1){
+//     console.log(numberss[index]+ numberss);
+// }
