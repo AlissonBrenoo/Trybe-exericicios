1947dbbda2bbdc8cdecb5d6036fa93636d8523da
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-03 18:13:09 -0300

    Jogo da mega-sena

--- Introdução ao JavaScrpit (aula ao vivo)/Dia-02-array-loop-for/loterryCheck.js
+++ Introdução ao JavaScrpit (aula ao vivo)/Dia-02-array-loop-for/loterryCheck.js
@@ -1,11 +1,29 @@
-let jogoDoChimba = [3,7,22,12,15,53];
+// Jogo do Chimba
 
-// console.log(jogoDoChimba);
-// console.log(jogoDoChimba[0]);
-// console.log(jogoDoChimba[1]);
-// console.log(jogoDoChimba[2]);
-// console.log(jogoDoChimba[3]);
-// console.log(jogoDoChimba[4]);
-// console.log(jogoDoChimba[5]);
+let studentGame = [3,7,22,12,15,53];
+console.log("Jogo do Chimba", studentGame);
 
-console.log(jogoDoChimba.length);
+// Jogo sorteado
+
+let megaSenaNumbers = [];
+for(let MegaSenaindex = 0; MegaSenaindex < 6; MegaSenaindex +=1){
+    let randomNumbers = Math.floor(Math.random()*60 + 1)
+    console.log(randomNumbers[MegaSenaindex]);
+}
+
+// Conferindo o jogo
+let numberOfHits = 0;
+for(let index = 0; index < megaSenaNumbers.length; index+=1){
+    let drawNumbers = megaSenaNumbers[index];
+    // console.log("Numero sorteado",drawNumbers);
+
+    for(let gameIndex = 0; gameIndex < studentGame.length; gameIndex+=1){
+        let studentNumber = studentGame[gameIndex]
+        // console.log("Numero do CHimba", studentGame[gameIndex]);
+
+    if (drawNumbers === studentNumber){
+        console.log("Numero de acertos", drawNumbers)
+    }
+
+    }
+}
