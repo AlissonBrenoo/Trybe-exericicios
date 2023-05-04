b357c29ca648a4a9f5002c1e940a1d4902300893
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-04 10:29:56 -0300

    Ajustando o codigo de forma mais efetiva

--- Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
@@ -1,13 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = [numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]];
-let divisao = soma / 10;
-
-if(divisao > 20){
-console.log("O valor da média aritmética é maior que 20");
-} 
-else{
-    console.log("O valor da média aritmética é menor ou igual a 20");
+let soma = 0;
+for (let index = 0; index < numbers.length; index+=1) {
+    soma += numbers[index];
 }
+console.log(soma / 10); 
+
+
+
+
+
+// if(divisao > 20){
+// console.log("O valor da média aritmética é maior que 20");
+// } 
+// else{
+//     console.log("O valor da média aritmética é menor ou igual a 20");
+// }
 
 
 // for(let index = 0; index < numberss.length; index+=1){
