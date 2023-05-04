34b0d1ff74a933cfdb878d1dde19573f67f63984
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-04 13:02:33 -0300

    terminando os exercicios de for

--- Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
@@ -1,7 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let maiorNum = 0
-let numImpar = 0
+let maiorNum = 0;
+let numImpar = 0;
+let minorValue = 0;
+let array1To25 = []; 
+
+
 for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index];
 }
@@ -10,7 +14,6 @@ if (soma > 20) {
 } else {
     console.log("O valor da média aritmética é menor ou igual a 20");
 }
-
 console.log('A média entre os números dos arrays é ' + soma / 10);
 
 
@@ -20,8 +23,17 @@ for (let index1 = 0; index1 < numbers.length; index1 += 1) {
         maiorNum = numbers[index1]
     }
 }
-
 console.log("O maior número é", maiorNum);
+
+
+minorValue = maiorNum;
+for (let index3 = 0; index3 < numbers.length; index3+=1) {
+    if (numbers[index3] < minorValue) {
+        minorValue = numbers[index3];
+    }
+}
+console.log('O menor número é', minorValue);
+
 
 for (let index2 = 0; index2 < numbers.length; index2 += 1) {
     if (numbers[index2] % 2 != 0) {
@@ -30,25 +42,19 @@ for (let index2 = 0; index2 < numbers.length; index2 += 1) {
 }
 
 if (numImpar > 0) {
-        console.log(numImpar);
+        console.log('Tem um total de', numImpar,'números impares');
 
     } else {
         console.log("Nenhum valor ímpar encontrado");
 }
 
 
+for (let index4 = 0; index4 < 25; index4+=1) {
+    array1To25[index4] = index4 +1;
+}
+console.log("Array de 1 a 25 ", array1To25);
 
-//  if(numbers[index1][index2] > maiorNum[index1]);{
-//             maiorNum[index1] = numbers[index1][index2];
-//         }
-// if(divisao > 20){
-// console.log("O valor da média aritmética é maior que 20");
-// } 
-// else{
-//     console.log("O valor da média aritmética é menor ou igual a 20");
-// }
-
-
-// for(let index = 0; index < numberss.length; index+=1){
-//     console.log(numberss[index]+ numberss);
-// }
+console.log("Divisão por 2 de todos os números do array acima");
+for (let index5 = 0; index5 < array1To25.length; index5+=1) {
+    console.log(array1To25[index5] / 2);
+}
