2ab9741f57ad33eed537c3f82bac9823649cb68b
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-04 11:54:58 -0300

    Fazendo o exercicio de numeros impares

--- Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
@@ -1,12 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let maiorNum= 0 
-for (let index = 0; index < numbers.length; index+=1) {
+let maiorNum = 0
+let numImpar = 0
+for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index];
 }
 if (soma > 20) {
     console.log("O valor da média aritmética é maior que 20");
-}else{
+} else {
     console.log("O valor da média aritmética é menor ou igual a 20");
 }
 
@@ -14,14 +15,26 @@ console.log('A média entre os números dos arrays é ' + soma / 10);
 
 
 
-for (let index1 = 0; index1 < numbers.length; index1+=1) {
-        if (numbers[index1] > maiorNum) {
-            maiorNum = numbers[index1]
-     }
+for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+    if (numbers[index1] > maiorNum) {
+        maiorNum = numbers[index1]
+    }
 }
 
 console.log("O maior número é", maiorNum);
 
+for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    if (numbers[index2] % 2 != 0) {
+        numImpar += 1
+    }
+}
+
+if (numImpar > 0) {
+        console.log(numImpar);
+
+    } else {
+        console.log("Nenhum valor ímpar encontrado");
+}
 
 
 
