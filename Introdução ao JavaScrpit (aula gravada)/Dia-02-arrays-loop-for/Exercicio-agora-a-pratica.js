575789f7269542628e9af0559fa58d1b86cdc08e
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-04 10:02:21 -0300

    conseguindo resolver algumas partes do exercicio

--- Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-02-arrays-loop-for/Exercicio-agora-a-pratica.js
@@ -1,7 +1,12 @@
-let numberss = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = []
-for(let index = 0; index < numberss.length; ){
-    console.log(numberss);
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = [numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]];
+let divisao = soma / 10;
+
+if(divisao > 20){
+console.log("O valor da média aritmética é maior que 20");
+} 
+else{
+    console.log("O valor da média aritmética é menor ou igual a 20");
 }
 
 
