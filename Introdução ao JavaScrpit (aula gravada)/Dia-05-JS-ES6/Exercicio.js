d085ad9522c932c950de0da40b1fcb070c0c5e21
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-08 21:46:56 -0300

    Iniciando 3 exercicio

--- Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
@@ -1,8 +1,21 @@
 //PARTE 1
-function imprimeIdade() {
-    for (var idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
-    console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+//     // console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+//   }
+//   imprimeIdade()
+
+//PARTE 2
+  // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+  let pessoa = {
+    nome: 'Henri',
+    idade: 20
   }
-  imprimeIdade()
+  pessoa = {
+    nome: 'Luna',
+    idade: 19
+  } // Altere essa estrutura para corrigir o erro.
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
