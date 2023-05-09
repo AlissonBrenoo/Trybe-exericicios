1970aea83e8e7e35cd3491f121822f0ec9593240
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-08 21:54:09 -0300

    Iniciando a parte 5

--- Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
@@ -8,14 +8,31 @@
 //   imprimeIdade()
 
 //PARTE 2
-  // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
-  let pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-  pessoa = {
-    nome: 'Luna',
-    idade: 19
-  } // Altere essa estrutura para corrigir o erro.
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+//   let pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa = {
+//     nome: 'Luna',
+//     idade: 19
+//   }
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+//PARTE 3
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+//PARTE 4
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
