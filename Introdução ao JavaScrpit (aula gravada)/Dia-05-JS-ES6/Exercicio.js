40236b4ccc3cb373fa68e7efaf3e40d352e7e3b6
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-08 22:06:49 -0300

    Iniciando a parte 7

--- Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
@@ -26,13 +26,23 @@
 
 //PARTE 4
 
-const name = 'Adriana';
-const lastName = 'Soares';
-console.log(`Olá, ${name} ${lastName}!`);
-function soma(a,b) {
-  let resultado = a + b;
-  return resultado;
-}
-let a = 3;
-let b = 5;
-console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log(`Olá, ${name} ${lastName}!`);
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+// //PARTE 5
+// const numeroAleatorio = () => Math.random()
+//   console.log(numeroAleatorio());
+
+
+//PARTE 6
+const hello = (nome) =>`Olá, ${nome}!`
+let nome = 'Ivan';
+console.log(hello(nome));
