78a7d2f56fa12d0710b5da78b02fc2f3aa5d33e2
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-11 18:29:57 -0300

    Criando pastas

--- Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
+++ Introdução ao JavaScrpit (aula gravada)/Dia-05-JS-ES6/Exercicio.js
@@ -54,17 +54,7 @@
 // console.log(nomeCompleto(nome, sobrenome));
 
 //PARTE 8
-let speed = 90;
-function speedCar(speed){
-  if (speed >= 120) {
-    return `Você ultrapassou o limite de velocidade`;
-  } else {
-    return `Você está na velocidade permitida`;
-  }
-};
-console.log(speedCar(speed));
-
 
 // let speed = 150;
-// const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` :`Você está na velocidade permitida`
-// console.log(speedCar(speed));
+const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` :`Você está na velocidade permitida`
+console.log(speedCar(speed));
