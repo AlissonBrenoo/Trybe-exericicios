1179d4a3fdfdba20572f5dae0abb55493c77b4d2
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-14 22:57:37 -0300

    Terminando o exercicio do tópico

---
+++ DOM-Eventos-e-Web-Storage-aula-gravada/Dia-01-DOM-e-seletores/Tópico 4/script.js
@@ -0,0 +1,13 @@
+document.getElementById('header-container').style.backgroundColor = 'Green';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'Pink';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'Purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.
+backgroundColor = 'Purple';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'Yellow';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'Black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'Black';
+
+document.querySelector('#footer-container').style.backgroundColor = 'DarkGreen'
