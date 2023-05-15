e2c2b00d130ddc57c5777cd76aa50103c39f3456
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-14 21:48:02 -0300

    Terminando o segundo praticar

--- DOM-Eventos-e-Web-Storage-aula-gravada/Dia-01-DOM-e-seletores/Primeiro praticar/script2.js
+++ DOM-Eventos-e-Web-Storage-aula-gravada/Dia-01-DOM-e-seletores/Primeiro praticar/script2.js
@@ -2,6 +2,6 @@ const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
 document.getElementById('page-title').innerText = "Jogador N°1";
-document.getElementById('paragraph').style.color = "blue";
+document.getElementById('paragraph').style.color = "darkBlue";
 document.getElementById('second-paragraph').innerText = "Em 2045, Wade Watts, assim como o resto da humanidade, prefere a realidade virtual do jogo OASIS ao mundo real. James Halliday, o excêntrico criador do jogo, morre e deixa sua fortuna inestimável para a primeira pessoa que descobrir a chave de um quebra-cabeça diabólico que ele arquitetou. Para vencer, Watts precisa abandonar a existência virtual e experimentar o amor e a realidade.";
 document.getElementById('subtitle').innerText = 'Resumo';
