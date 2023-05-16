d35ebc2169a857c467702f65feaa2386893dc307
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-15 21:04:36 -0300

    terminando exercicio do primeiro topico

--- DOM-Eventos-e-Web-Storage-aula-gravada/Dia-02-DOM-e-seletores/Topico 1/script.js
+++ DOM-Eventos-e-Web-Storage-aula-gravada/Dia-02-DOM-e-seletores/Topico 1/script.js
@@ -1,5 +1,17 @@
-let parentWhereAreYOu = document.querySelector('#where-are-you').parentNode
-document.querySelector('#parent').style.backgroundColor = ('Green');
+let WhereAreYOu = document.querySelector('#where-are-you');
 
-let childWhereAreYou = document.querySelector('#where-are-you').childNodes;
-childWhereAreYou = 'dhaudhasdsjk'
+let parentWhereAreYOu = document.querySelector('#where-are-you').
+parentNode;
+parentWhereAreYOu.style.backgroundColor = ('Green');
+
+let childWhereAreYou = document.querySelector('#where-are-you').firstElementChild;
+childWhereAreYou.innerText = 'Novo novinho';
+
+let firstChild = document.querySelector('#parent').firstElementChild
+firstChild = document.querySelector('#where-are-you').previousElementSibling
+
+let paragraph = document.querySelector('#where-are-you').nextSibling;
+
+let thirdChild = document.querySelector('#where-are-you').nextElementSibling
+thirdChild = document.querySelector('#parent').childNodes[5];
+console.log(thirdChild);
