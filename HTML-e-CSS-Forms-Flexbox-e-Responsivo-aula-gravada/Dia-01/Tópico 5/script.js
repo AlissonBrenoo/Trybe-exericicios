43845d420c495b06233afdecb06518fc3ed6ec86
Alisson Breno <alissonbreno36@hotmail.com>
2023-05-19 19:40:15 -0300

    Finalizando o tópico

--- HTML-e-CSS-Forms-Flexbox-e-Responsivo-aula-gravada/Dia-01/Tópico 5/script.js
+++ HTML-e-CSS-Forms-Flexbox-e-Responsivo-aula-gravada/Dia-01/Tópico 5/script.js
@@ -3,10 +3,17 @@ const inputText = document.querySelector("#input-text");
 const inputCheckbox = document.querySelector("#input-checkbox");
 const hrefLink = document.querySelector("#href");
 
-const clickA = (action) => {
-    action.preventDefault();
-    console.log(hrefLink.value);
+hrefLink.addEventListener('click', (event) => {
+    event.preventDefault();
 
+} );
+inputCheckbox.addEventListener('click', (Event) =>{
+    Event.preventDefault();
+})
+
+inputText.addEventListener('keypress', (newEvent) =>{
+const key = event.key
+if(key !== 'a'){
+    event.preventDefault();
 } 
-clickA();
-hrefLink.addEventListener('click', clickA);
+}) 
