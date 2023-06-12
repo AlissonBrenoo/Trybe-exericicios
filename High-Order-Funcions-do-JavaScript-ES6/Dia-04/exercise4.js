e4a42a2db44cb46b8a2d01e9e666b63899654e8a
Alisson Breno <alissonbreno36@hotmail.com>
2023-06-12 20:14:28 -0300

    Finalizando 4 exercicio

---
+++ High-Order-Funcions-do-JavaScript-ES6/Dia-04/exercise4.js
@@ -0,0 +1,32 @@
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+    // bornIn: nascido em
+  ];
+  
+  // escreva filterPeople abaixo
+  const filterPeople = people.filter((peoples) => peoples.bornIn >= 1999)
+  console.log(filterPeople);
